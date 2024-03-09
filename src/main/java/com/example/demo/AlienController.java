916fe5c075699795afac61bd6c605477770aa73e
kalingam123 <95062205+kalingam123@users.noreply.github.com>
2024-03-09 16:51:30 +0530

    Added the SpringBoot JPA with REST-API

---
+++ src/main/java/com/example/demo/AlienController.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AlienController {
+
+	@Autowired
+	private AlienRepository alienRepository;
+	
+	List<Alien> aliens=new ArrayList<>();
+	
+	@RequestMapping("aliens")
+	public List<Alien> getAliens(){
+		Alien a=new Alien();
+		a.setId(101);
+		a.setName("Kamal");
+		a.setPoints(25);
+		
+		Alien a1=new Alien();
+		a1.setId(102);
+		a1.setName("Pooja");
+		a1.setPoints(35);
+		aliens.add(a);
+		aliens.add(a1);
+		return aliens;
+	}
+	
+	@GetMapping("jpa/aliens")
+	public List<Alien> getAlien(){
+		List<Alien> aliens=(List<Alien>) alienRepository.findAll();
+		return aliens;
+	}
+}
